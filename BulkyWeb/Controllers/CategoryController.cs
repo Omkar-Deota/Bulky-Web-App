d05d21863e5d93cfbea8922e64b6a22fbe6d7fd7
Omkar-Deota <omkarinshu2003@gmail.com>
2025-06-30 19:21:13 +0530

    Initialized Project

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _context = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _context.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+
+}
+
